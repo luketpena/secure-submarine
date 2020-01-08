1de5dff9d807ba60919faf5513e1fd9c6e7130c3
Luke Peña <luketpena@gmail.com>
2020-01-08 10:12:39 -0600

    Completed stretch goals up to Heroku deployment

---
+++ build/precache-manifest.98c8ff7485f91da1c49d0130dd62440d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad84c7e0c68311740a6053e3827a425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dc64f87fc058be3a9fd",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "b048eb57650f5ebe595a",
+    "url": "/static/js/2.2d180690.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2d180690.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dc64f87fc058be3a9fd",
+    "url": "/static/js/main.1a612ccd.chunk.js"
+  },
+  {
+    "revision": "e264d7abc61669920fa8",
+    "url": "/static/js/runtime-main.f0b207d1.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
